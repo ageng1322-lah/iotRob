8ad37f0b063303ff9bbc68eeedd4b9cc42bdd5ea
ageng1322-lah <agengguna26@gmail.com>
2026-02-11 20:50:50 +0700

    first commit

--- api.php
+++ api.php
@@ -17,11 +17,11 @@ if ($conn->connect_error) {
 // Contoh URL: http://domain.com/api.php?kualitas_air=10&tahan=20&udara=30&daya_listrik=40
 
 if (isset($_GET['kualitas_air']) && isset($_GET['tahan']) && isset($_GET['udara']) && isset($_GET['daya_listrik'])) {
-    
-    $kualitas_air = (float)$_GET['kualitas_air'];
-    $tahan = (float)$_GET['tahan'];
-    $udara = (float)$_GET['udara'];
-    $daya_listrik = (float)$_GET['daya_listrik'];
+
+    $kualitas_air = (float) $_GET['kualitas_air'];
+    $tahan = (float) $_GET['tahan'];
+    $udara = (float) $_GET['udara'];
+    $daya_listrik = (float) $_GET['daya_listrik'];
 
     // Query untuk menyimpan data
     $sql = "INSERT INTO sensor_logs (kualitas_air, tahan, udara, daya_listrik)
@@ -33,7 +33,17 @@ if (isset($_GET['kualitas_air']) && isset($_GET['tahan']) && isset($_GET['udara'
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 } else {
-    echo "Parameter tidak lengkap. Harap kirimkan parameter: kualitas_air, tahan, udara, daya_listrik";
+    // Mode Read: Ambil data terakhir dalam format JSON untuk frontend
+    header('Content-Type: application/json');
+    $sql = "SELECT * FROM sensor_logs ORDER BY id DESC LIMIT 1";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        echo json_encode($row);
+    } else {
+        echo json_encode(["error" => "No data found"]);
+    }
 }
 
 $conn->close();
